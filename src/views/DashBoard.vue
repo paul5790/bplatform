01d49503145a23b8d37775b728c2e683513bfa23
hyunmin kim <paul5790@naver.com>
2023-09-21 14:16:27 +0900

    230921.1416

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -7,6 +7,18 @@
     <h1>Dashboard</h1>
     <h1>Dashboard</h1>
     <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
   </div>
 </template>
 
