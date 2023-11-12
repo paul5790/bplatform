395b689ce10eec5c229c461162bef26a80925346
paul <paul5790@naver.com>
2023-11-12 22:57:00 +0900

    231112.2256

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -1,19 +1,17 @@
 <template>
-  <v-row>
-    <v-col>
-      <!-- <v-sheet style="height: 50vh">
+    <v-row style="height: 45vh; background-color: #f7f7f7;">
+      <v-col cols="4" style="background-color: #f7f7f7;">
+        <!-- <v-sheet style="height: 50vh">
         <ex1 />
       </v-sheet> -->
-      <v-sheet
-        style="height: 50vh; padding: 10px; padding-right: 0; display: flex"
-      >
-        <v-card :color="primary" :variant="elevated" style="flex: 1">
-          <v-card-item>
-            <VoyageTable />
-          </v-card-item>
-        </v-card>
-      </v-sheet>
-      <v-sheet
+        <v-sheet style="height: 45vh; padding: 10px; padding-right: 0; display: flex; background-color: #f7f7f7;">
+          <v-card :color="primary" :variant="elevated" style="flex: 1">
+            <v-card-item>
+              <EchartDrillDown />
+            </v-card-item>
+          </v-card>
+        </v-sheet>
+        <!-- <v-sheet
         style="height: 50vh; padding: 10px; padding-right: 0; display: flex"
       >
         <v-card :color="primary" :variant="elevated" style="flex: 1">
@@ -21,62 +19,39 @@
             <SaillingAnimation />
           </v-card-item>
         </v-card>
-      </v-sheet>
-    </v-col>
-    <v-col cols="4" sm="12" md="6" lg="4">
-      <v-sheet
-        style="
-          height: 50vh;
-          padding: 10px;
-          padding-right: 0;
-          padding-left: 0;
-          display: flex;
-        "
-      >
-        <v-card :color="primary" :variant="elevated" style="flex: 1">
-          <v-card-item>
-            <EchartBar />
-          </v-card-item>
-        </v-card>
-      </v-sheet>
-      <v-sheet
-        style="
-          height: 50vh;
-          padding: 10px;
-          padding-right: 0;
-          padding-left: 0;
-          display: flex;
-        "
-      >
-        <v-card :color="primary" :variant="elevated" style="flex: 1">
-          <v-card-item>
-            <EchartDrillDown />
-          </v-card-item>
-        </v-card>
-      </v-sheet>
-    </v-col>
-    <v-col cols="4" sm="12" md="6" lg="4">
-      <v-sheet
-        style="height: 50vh; padding: 10px; padding-left: 0; display: flex"
-      >
-        <v-card :color="primary" :variant="elevated" style="flex: 1">
-          <v-card-item>
-            <EchartDoughnut />
-          </v-card-item>
-        </v-card>
-      </v-sheet>
-      <v-sheet
-        style="height: 50vh; padding: 10px; padding-left: 0; display: flex"
-      >
-        <v-card :color="primary" :variant="elevated" style="flex: 1">
-          <v-card-item>
-            <EchartHalfDoughnut />
-          </v-card-item>
-        </v-card>
-      </v-sheet>
-    </v-col>
+      </v-sheet> -->
+      </v-col>
+      <v-col cols="4" style="background-color: #f7f7f7;">
+        <v-sheet
+          style="
+          height: 45vh;
+            padding: 10px;
+            padding-right: 0;
+            padding-left: 0;
+            display: flex;
+            background-color: #f7f7f7;
+          "
+        >
+          <v-card :color="primary" :variant="elevated" style="flex: 1">
+            <v-card-item>
+              <EchartDoughnut />
+            </v-card-item>
+          </v-card>
+        </v-sheet>
+      </v-col>
+      <v-col cols="4" style="background-color: #f7f7f7;">
+        <v-sheet
+          style="height: 45vh; padding: 10px; padding-left: 0; display: flex; background-color: #f7f7f7;"
+        >
+          <v-card :color="primary" :variant="elevated" style="flex: 1">
+            <v-card-item>
+              <EchartHalfDoughnut />
+            </v-card-item>
+          </v-card>
+        </v-sheet>
+      </v-col>
 
-    <!-- <v-col cols="4">
+      <!-- <v-col cols="4">
       <v-sheet class="bg-surface-variant" >
         <SaillingAnimation />
       </v-sheet>
@@ -87,7 +62,18 @@
     <v-col cols="4">
       <v-sheet style="height: 50vh"> .v-col-auto </v-sheet>
     </v-col> -->
-  </v-row>
+    </v-row>
+    <v-row>
+      <v-col style="background-color: #f7f7f7;">
+        <v-sheet style="height: 55vh; padding: 10px; background-color: #f7f7f7;">
+          <v-card :color="primary" :variant="elevated" style="flex: 1">
+            <v-card-item>
+              <VoyageTable />
+            </v-card-item>
+          </v-card>
+        </v-sheet>
+      </v-col>
+    </v-row>
 </template>
 
 <script setup>
